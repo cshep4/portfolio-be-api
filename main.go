337b9dd0a90c689b2dec3595b780fa915e9f158e
cshep4 <chris_shepherd2@hotmail.com>
2018-08-28 23:25:32 +0100

    Removed sms boilerplate service

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
-	"portfolio-be-api/app/sms"
 	"portfolio-be-api/app/service"
 	"portfolio-be-api/app/email"
 	"os"
@@ -12,7 +11,6 @@ import (
 func main() {
 	router := mux.NewRouter()
 
-	service.Route(router, new(sms.SmsService))
 	service.Route(router, new(email.EmailService))
 
 	http.ListenAndServe(":" + os.Getenv("PORT"), router)
