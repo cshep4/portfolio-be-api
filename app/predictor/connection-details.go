29decb9460fd33a5fffe8c90751d8f2b04ba7d3b
cshep4 <chris_shepherd2@hotmail.com>
2018-09-02 22:26:01 +0100

    changed mistake in string

--- app/predictor/connection-details.go
+++ app/predictor/connection-details.go
@@ -3,7 +3,7 @@ package predictor
 import "os"
 
 func getDbCredentials(appName string) string {
-	if appName == "PremiersPredictor" {
+	if appName == "PremierPredictor" {
 		return os.Getenv("DATABASE_URL")
 	} else {
 		return os.Getenv("HEROKU_POSTGRESQL_GRAY_URL")
